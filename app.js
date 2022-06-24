8691231758d7e0609d71cfe02b10e7c96b2fa2a4
John Crossman <jgcrossman2@gmail.com>
2022-06-24 11:37:35 -0700

    setting up the framework

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+const session = require('express-session');
+const path = require('path');
+// const checkAuthorization = required('./utils/authorization');
+
+// const userRoutes = require('./routes/users');
+// const indexRoutes = require('./routes/index');
+
+// const PORT = 3000;
+// const CONNECTION_STRING = "postgres://localhost:5432/newsdb";
+
+const VIEWS_PATH = path.join(__dirname,'/views');
+
+app.engine('mustache',mustacheExpress(VIEWS_PATH + '/partials','.mustache'));
+app.set('views',VIEWS_PATH);
+app.set('view engine','mustache');
+app.use('/css',express.static('css'));
+
+app.use(session({
+    secret: 'lgbtqwerty',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use((req,res,next) => {
+    res.locals.authenticated = req.session.user == null ? false : true
+    next()
+});
+
+// if the "const" is removed, db becomes available to users.js
+db = pgp(CONNECTION_STRING);
+
+// set up a middleware for routes
+app.use('/',indexRoutes);
+app.use('/users',checkAuthorization,userRoutes);
+
+app.listen(PORT,() => {
+    console.log(`Server has started on ${PORT}`)
+});
