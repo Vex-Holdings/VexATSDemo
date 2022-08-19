0f6f6a0b96f64bc0e778ba630e8533497cf4ad68
John Crossman <jgcrossman2@gmail.com>
2022-08-18 17:54:55 -0700

    Playing with user routes

--- routes/users.js
+++ routes/users.js
@@ -17,8 +17,15 @@ router.get('/dashboard', async (req,res) => {
     })
 
     let name = user.firstname
-    console.log(user)
-    res.send(`Hi ${name}! You have reached the dashboard page.`)
+    let status = user.status
+    let accounttype = user.accounttype
+    // console.log(user)
+    if(status == null) {
+        res.send(`Hi ${name}! The next step is to fill out the ${accounttype} <a href="/users/personal">new account form</a>.`)
+    } else {
+        res.send(`Hi ${name}! You have reached the dashboard page. You are ready for the order or <a href="/users/market">market page</a>`)
+    }
+    
 })
 
 router.get('/market', async (req,res) => {
@@ -31,6 +38,18 @@ router.get('/market', async (req,res) => {
     })
     let name = user.firstname
     res.send(`Hi, ${name}, welcome to the market page`)
+})
+
+router.get('/personal', async (req,res) => {
+    let session = req.session
+    let id = session.user.userId
+    let user = await models.User.findOne({
+        where: {
+            id: id
+        }
+    })
+    let name = user.firstname + " " + user.lastname
+    res.send(`Hi, ${name}, this is where we will put the new account form`)
 })
 
 // POST Pages
