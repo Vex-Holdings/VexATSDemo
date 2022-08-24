584e02c6cf544c29464767eb6e7074187d35dbfd
John Crossman <jgcrossman2@gmail.com>
2022-08-24 15:19:21 -0700

    create POST action for Personal Account form

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,7 @@ router.get('/dashboard', async (req,res) => {
     let status = user.status
     let accounttype = user.accounttype
     // console.log(user)
-    if(status == null) {
+    if(status == 'user') {
         res.send(`Hi ${name}! The next step is to fill out the ${accounttype} <a href="/users/personal">new account form</a>.`)
     } else {
         res.send(`Hi ${name}! You have reached the dashboard page. You are ready for the order or <a href="/users/market">market page</a>`)
@@ -52,5 +52,113 @@ router.get('/personal', async (req,res) => {
 })
 
 // POST Pages
+
+router.post('/personal', async (req,res) => {
+    let userid = req.body.userid
+    let addressline1 = req.body.addressline1
+    let addressline2 = req.body.addressline2
+    let addresscity = req.body.addresscity
+    let addressstate = req.body.addressstate
+    let addresszip = req.body.addresszip
+    let maritalstatus = req.body.maritalstatus
+    let dependants = req.body.dependants
+    let employmentstatus = req.body.employmentstatus
+    let jobtitle = req.body.jobtitle
+    let occupation = req.body.occupation
+    let employername = req.body.employername
+    let employertime = req.body.employertime
+    let employeraddress = req.body.employeraddress
+    let employersuite = req.body.employersuite
+    let employercity = req.body.employercity
+    let employerstate = req.body.employerstate
+    let employerzip = req.body.employerzip
+    let dateofbirth = req.body.dateofbirth
+    let citizenship = req.body.citizenship
+    let taxidtype = req.body.taxidtype
+    let taxidnumber = req.body.taxidnumber
+    let identitydocument = req.body.identitydocument
+    let identityissuer = req.body.identityissuer
+    let identityidnumber = req.body.identityidnumber
+    let identityissuedate = req.body.identityissuedate
+    let identityexpiredate = req.body.identityexpiredate
+    let accounttype = req.body.accounttype
+    let customertype = req.body.customertype
+    let associated = req.body.associated
+    let associatedtype = req.body.associatedtype
+    let associatedorganization = req.body.associatedorganization
+    let pubco = req.body.pubco
+    let pubconame = req.body.pubconame
+    let pep = req.body.pep
+    let pepcountry = req.body.pepcountry
+    let investmentobjective = req.body.investmentobjective
+    let investmentexperience = req.body.investmentexperience
+    let risktolerance = req.body.risktolerance
+    let timehorizon = req.body.timehorizon
+    let liquidity = req.body.liquidity
+    let income = req.body.income
+    let networth = req.body.networth
+    let taxstatus = req.body.taxstatus
+    let status = 'submitted'
+
+    let personal = models.Personal.build({
+        userid: userid,
+        addressline1: addressline1,
+        addressline2: addressline2,
+        addresscity: addresscity,
+        addressstate: addressstate,
+        addresszip: addresszip,
+        maritalstatus: maritalstatus,
+        dependants: dependants,
+        employmentstatus: employmentstatus,
+        jobtitle: jobtitle,
+        occupation: occupation,
+        employername: employername,
+        employertime: employertime,
+        employeraddress: employeraddress,
+        employersuite: employersuite,
+        employercity: employercity,
+        employerstate: employerstate,
+        employerzip: employerzip,
+        dateofbirth: dateofbirth,
+        citizenship: citizenship,
+        taxidtype: taxidtype,
+        taxidnumber: taxidnumber,
+        identitydocument: identitydocument,
+        identityissuer: identityissuer,
+        identityidnumber: identityidnumber,
+        identityissuedate: identityissuedate,
+        identityexpiredate: identityexpiredate,
+        accounttype: accounttype,
+        customertype: customertype,
+        associated: associated,
+        associatedtype: associatedtype,
+        associatedorganization: associatedorganization,
+        pubco: pubco,
+        pubconame: pubconame,
+        pep: pep,
+        pepcountry: pepcountry,
+        investmentobjective: investmentobjective,
+        investmentexperience: investmentexperience,
+        risktolerance: risktolerance,
+        timehorizon: timehorizon,
+        liquidity: liquidity,
+        income: income,
+        networth: networth,
+        taxstatus: taxstatus
+    })
+    let persistedPersonal = await personal.save()
+    const submitted = await models.User.update({
+        status: status
+    },{
+        where: {
+            id: userid
+        }
+    })
+    if(persistedPersonal != null) {
+        res.redirect('/users/dashboard')
+    } else {
+        res.render('users/personal',{message: 'Unable to submit new account form'})
+    }
+})
 
 module.exports = router;
