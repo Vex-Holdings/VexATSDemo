81eab41791a974757b68c6e58e416b4a923279a8
John Crossman <jgcrossman2@gmail.com>
2022-09-16 17:16:28 -0700

    starting cleanup on control panel

--- routes/users.js
+++ routes/users.js
@@ -102,13 +102,7 @@ router.get('/dashboard', async (req,res) => {
     let holdings = await sequelize.query('SELECT m.holding, m.status, s.name FROM "Mshfs" m JOIN "Stocks" s ON m.stockid = s.id WHERE m.userid = ' + id, {type: Sequelize.QueryTypes.SELECT})
     
     let orders = await sequelize.query('SELECT o.type, o.size, o.price, s.name FROM "Orders" o JOIN "Stocks" s ON o.stockid = s.id WHERE o.userid = ' + id, {type: Sequelize.QueryTypes.SELECT})
-    /*
-    models.Order.findAll({
-        where: {
-            userid: id
-        }
-    })
-    */
+    
     let bids = await models.Order.findAll({
         where: {
             stockid: 1,
